290a02909f249101ebeec0e26a6f733a6d7bdff0
irayshi <raffiirayshi@gmail.com>
2024-11-28 13:47:11 +0700

    test lagi

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"36335019a8eab588c3c2ea783c618d90505be2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2913647712"
+    serviceWorkerVersion: "4285511381"
   }
 });
